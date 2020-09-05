eee90a29a9f963c5a9761cef3c754d43220011c5
flyingImer <ej.wang.dev@gmail.com>
2020-09-05 15:35:43 -0700

    chore(setup): prepare for cdk env

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,28 @@
-const { ConstructLibraryAws } = require('projen');
+const { ConstructLibraryAws, Stability, Semver } = require('projen');
 
 const project = new ConstructLibraryAws({
-  "name": "cdk-distributed-computing",
-  "authorName": "Ej Wang",
-  "authorEmail": "ej.wang.dev@gmail.com",
-  "repository": "https://github.com/flyingImer/cdk-distributed-computing.git"
+  name: 'cdk-distributed-computing',
+
+  stability: Stability.Experimental,
+
+  authorName: 'Ej Wang',
+  authorEmail: 'ej.wang.dev@gmail.com',
+  repository: 'https://github.com/flyingImer/cdk-distributed-computing.git',
+
+  cdkVersion: '1.32.2',
+  cdkDependencies: [
+    '@aws-cdk/aws-dynamodb',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-lambda-event-sources',
+    '@aws-cdk/aws-sqs',
+    "@aws-cdk/core"
+  ],
+
+  devDependencies: {
+    "aws-sdk": Semver.caret("2.708.0")
+  }
 });
+
+project.gitignore.exclude('.env');
 
 project.synth();
